8280961e9054b243e93348e647dcb00e51f394dc
gauravKothari <kotharigaurav6@gmail.com>
2026-04-13 11:45:36 +0530

    react

---
+++ Web Designing/ReactJs/13 Life cycle methods of class component/src/index4.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {createRoot} from 'react-dom/client';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {favoritecolor: "red"};
+  }
+  static getDerivedStateFromProps(props, state) {
+    return {favoritecolor: props.favcol };
+  }
+  changeColor = () => {
+    setTimeout(()=>{
+      this.setState({favoritecolor: "blue"});
+    },2000);
+  }
+  render() {
+    return (
+      <div>
+      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+      <button type="button" onClick={this.changeColor}>Change color</button>
+      </div>
+    );
+  }}
+
+createRoot(document.getElementById("root")).render(<Header favcol="yellow"/>);
